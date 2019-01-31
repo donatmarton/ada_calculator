6e66abef32740f143047c298b89ded715fb45386
donatmarton <donat@donatmarton.com>
2019-01-31 20:20:13 +0100

    add bad input warning printer

--- src/commandlineoutput.adb
+++ src/commandlineoutput.adb
@@ -6,15 +6,21 @@ package body CommandLineOutput is
    begin
       Put_Line ("Welcome to basic expression calculator!");
    end Print_Welcome_Message;
-   
+
    procedure Print_Farewell_Message is
    begin
       Put_Line ("Goodbye.");
    end Print_Farewell_Message;
 
-   procedure Print_Custom_Message (message : String) is
+   procedure Print_Custom_Message (Message : String) is
    begin
-      Put_Line (message);
+      Put_Line (Message);
    end Print_Custom_Message;
+
+   procedure Print_Illegal_Input_Error is
+   begin
+      Put_Line ("Error, illegal characters in input. " &
+        "Only numbers and the four elementary operations are permitted.");
+   end Print_Illegal_Input_Error;
 
 end CommandLineOutput;
