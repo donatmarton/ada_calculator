9326498007163ec0fbd6537cecbe404b8b07c36c
donatmarton <donat@donatmarton.com>
2019-01-31 20:21:14 +0100

    check input for illegal chars

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,10 @@
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Strings.Maps; use Ada.Strings.Maps;
+with Ada.Strings.Fixed;
 with CommandLineOutput;
 with Parser;
 
 procedure Main is
+   Legal_Characters : constant Character_Set := To_Set ("0123456789+-*/");
 begin
    CommandLineOutput.Print_Welcome_Message;
    loop
@@ -13,7 +15,14 @@ begin
             CommandLineOutput.Print_Farewell_Message;
             exit;
          else
-            CommandLineOutput.Print_Custom_Message (User_Input);
+            --  check for sanity of user input
+            if (Ada.Strings.Fixed.Count
+                (User_Input, Legal_Characters) = User_Input'Length)
+            then
+               CommandLineOutput.Print_Custom_Message (User_Input);
+            else
+               CommandLineOutput.Print_Illegal_Input_Error;
+            end if;
          end if;
       end;
    end loop;
