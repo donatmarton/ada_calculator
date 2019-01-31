114ee4ab0e9836d9af66e1883839ccf6e8aeba15
donatmarton <donat@donatmarton.com>
2019-01-31 21:47:12 +0100

    add types for expression processing

--- src/parser.ads
+++ src/parser.ads
@@ -1,5 +1,11 @@
 package Parser is
 
    function Parse_For_Exit_Command (Line_To_Parse : String) return Boolean;
+   subtype Operation_Count is Integer range 0 .. 4;
+   type Sign is (Positive, Negative);
+   type Subexpression is record
+      Expression_String : access String;
+      Leading_Sign : Sign;
+   end record;
 
 end Parser;
